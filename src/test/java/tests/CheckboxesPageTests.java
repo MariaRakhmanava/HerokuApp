5ad7f2ea19544ed24248432c4fbc85e87adb2009
MariaRakhmanava <mariah.rakhmanava@gmail.com>
2021-05-07 19:35:17 +0300

    Added java file for Checkboxes page test

---
+++ src/test/java/tests/CheckboxesPageTests.java
@@ -0,0 +1,30 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class CheckboxesPageTests {
+
+    @Test
+    public void checkbox1Test() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get("http://the-internet.herokuapp.com/checkboxes");
+        List<WebElement> checkboxes = driver.findElements(By.cssSelector("[type=checkbox]"));
+        Assert.assertFalse(checkboxes.get(0).isSelected());
+        checkboxes.get(0).click();
+        Assert.assertTrue(checkboxes.get(1).isSelected());
+        checkboxes.get(1).click();
+        Assert.assertFalse(checkboxes.get(1).isSelected());
+        // driver.quit();
+    }
+}
